24dc45460fcee36090b9600508f4715dad0c48ff
Sergio Lopez Rey <sergiolopezrey@hotmail.com>
2021-07-31 09:30:34 -0300

    Chequeos registro

--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -2,8 +2,24 @@ const router = require('express').Router();
 const User = require('../Schemas/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const Joi = require('@hapi/joi');
+
+const schemaRegister = Joi.object({
+  name: Joi.string().min(1).max(35).required(),
+  surname: Joi.string().min(1).max(35).required(),
+  email: Joi.string().min(6).max(255).required().email().messages({
+    'string.email': 'El campo email tiene que tener un email valido',
+  }),
+  password: Joi.string().min(6).max(1024).required(),
+});
 router.post('/', async (req, res) => {
-  const findEmail = await User.findOne({ email: req.body.user.email });
+  const { error } = schemaRegister.validate(req.body.user);
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+  const findEmail = await User.findOne({
+    email: req.body.user.email.toLowerCase(),
+  });
 
   if (findEmail) {
     return res.status(400).json({ error: 'email ya  registrado' });
@@ -14,9 +30,9 @@ router.post('/', async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const passwordSalted = await bcrypt.hash(req.body.user.password, salt);
     const registerUser = new User({
-      name: req.body.user.name,
-      surname: req.body.user.surname,
-      email: req.body.user.email,
+      name: req.body.user.name.toLowerCase(),
+      surname: req.body.user.surname.toLowerCase(),
+      email: req.body.user.email.toLowerCase(),
       password: passwordSalted,
     });
     const savedUser = await registerUser.save();
