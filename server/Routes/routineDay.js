e765b317eb02835de56fa01d8964753552be8780
Sergio Lopez Rey <sergiolopezrey@hotmail.com>
2021-08-13 02:01:55 -0300

    Now you can open routineDay

--- server/Routes/routineDay.js
+++ server/Routes/routineDay.js
@@ -43,11 +43,61 @@ router.put("/:id", async (req, res) => {
       { new: true, useFindAndModify: false }
     );
     if (!diaRutinaEjercicioAgregado) {
-      res.status(202).json("No se encontro el dia de rutina");
+      return res.status(202).json("No se encontro el dia de rutina");
+    } else {
+      const resultado = await Routine.findOne({
+        _id: diaRutinaEjercicioAgregado.routineId,
+      }).populate("entrenamientoDias");
+
+      res.status(200).json({
+        rutinasActualizadas: resultado,
+        mensaje: "Se agrego correctamente",
+      });
     }
+  } catch (error) {
+    res.status(400).json({ mensaje: "Hay algun error con la query" });
+  }
+});
+router.delete("/ejercicio/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const ejercicioAEliminar = req.body.ejercicio;
+    const diaDeEjercicio = await TrainingRoutine.findOneAndUpdate(
+      { _id: id },
+      { $pull: { ejercicios: ejercicioAEliminar } },
+      { new: true, useFindAndModify: false }
+    );
+    if (!diaDeEjercicio) {
+      return res.status(202).json("No se encontro el dia de rutina");
+    }
+
     res.status(200).json({
       rutinaNueva: diaRutinaEjercicioAgregado,
       mensaje: "Se agrego correctamente",
+    });
+  } catch (error) {
+    res.status(400).json({ mensaje: "Hay algun error con la query" });
+  }
+});
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const diaDeRutina = req.body;
+
+    const result = await TrainingRoutine.deleteOne({ _id: id });
+    if (!result) {
+      return res
+        .status(202)
+        .json({ mensaje: "no se encontro ningun dia para eliminar" });
+    }
+    const rutinaActualizada = await Routine.findOneAndUpdate(
+      { _id: diaDeRutina.routineId },
+      { $pull: { entrenamientoDias: id } },
+      { new: true, useFindAndModify: false }
+    );
+    res.status(200).json({
+      rutina: rutinaActualizada,
+      mensaje: "Dia de entremiento eliminado con exito",
     });
   } catch (error) {
     res.status(400).json({ mensaje: "Hay algun error con la query" });
--- server/Routes/routines.js
+++ server/Routes/routines.js
@@ -1,9 +1,9 @@
-const router = require('express').Router();
-const Routine = require('../Schemas/routine');
-const TrainingRoutine = require('../Schemas/traininigRoutine');
-const jwt = require('jsonwebtoken');
+const router = require("express").Router();
+const Routine = require("../Schemas/routine");
+const TrainingRoutine = require("../Schemas/traininigRoutine");
+const jwt = require("jsonwebtoken");
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const nombreRutina = req.body.name;
     const idUsuario = req.user.id;
@@ -11,7 +11,7 @@ router.post('/', async (req, res) => {
       if (result.length > 0) {
         res
           .status(400)
-          .json({ error: 'Ya tienes una rutina con ese nombre' })
+          .json({ error: "Ya tienes una rutina con ese nombre" })
           .end();
       } else {
         const rotineReq = new Routine({
@@ -19,38 +19,38 @@ router.post('/', async (req, res) => {
           userId: idUsuario,
         });
         rotineReq.save();
-        res.status(200).json('Rutina guardada').end();
+        res.status(200).json("Rutina guardada").end();
       }
     });
   } catch (error) {
-    console.log('ocurrio un error');
-    res.status(400).json({ error: 'Faltan parametros en la query' }).end();
+    console.log("ocurrio un error");
+    res.status(400).json({ error: "Faltan parametros en la query" }).end();
   }
 });
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   const id = req.user.id;
   const resultado = await Routine.find({
     userId: id,
-  }).populate('entrenamientoDias');
+  }).populate("entrenamientoDias");
 
   if (resultado.length > 0) {
     res.status(200).json(resultado).end();
   } else {
     res
       .status(404)
-      .json({ error: 'No se encontraron rutinas para este usuario' })
+      .json({ error: "No se encontraron rutinas para este usuario" })
       .end();
   }
 });
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     const id = req.params.id;
     const encontrada = await Routine.findOne({ _id: id });
     if (!encontrada) {
       return res
         .status(202)
-        .json({ mensaje: 'no se encontro la rutina a borrar' });
+        .json({ mensaje: "no se encontro la rutina a borrar" });
     } else {
       encontrada.entrenamientoDias.forEach((e) => {
         const borrarDia = TrainingRoutine.deleteOne(
@@ -60,13 +60,19 @@ router.delete('/:id', async (req, res) => {
           }
         );
       });
-      encontrada.delete();
+      await encontrada.delete();
     }
+    const userId = req.user.id;
+    const resultado = await Routine.find({
+      userId: userId,
+    }).populate("entrenamientoDias");
 
-    res.status(200).json({ mensaje: 'Rutina borrada con exito' });
+    res
+      .status(200)
+      .json({ rutinas: resultado, mensaje: "Rutina borrada con exito" });
   } catch (error) {
     console.log(error);
-    res.status(400).json({ mensaje: 'hay un error en la query' });
+    res.status(400).json({ mensaje: "hay un error en la query" });
   }
 });
 
